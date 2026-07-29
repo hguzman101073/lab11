42251bcc181e07c63b4a54317548c074f016a50e
Henry Guzman <hguzman008@gmail.com>
2026-07-29 12:02:33 -0400

    Modified Calculator p1

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
 #Henry Guzmans Git Code
+import math
+
+def add(a,b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a , b):
+    if a == 0:
+        raise ZeroDivisionError("The first number a cannot be 0")
+    return b / a
+def logarithm(a, b):
+    if a <=0:
+        raise ValueError("Input must be greater than 0")
+    return log(b, a)
+def exponent(a, b):
+    return ab
