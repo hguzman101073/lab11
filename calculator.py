4aef7354ed1d59890c4ede147d48a83e12b9a98b
Henry Guzman <hguzman008@gmail.com>
2026-07-29 12:18:24 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,19 +1,32 @@
 #Henry Guzmans and Joseph Menzos Git Code for calculator.py
 import math
+def square_root(a):
+    try:
+        math.sqrt(a)
+    except ValueError:
+        print("Error: Cannot calculate of a negative number")
+
+def hypotenuse(a,b):
+    math.hypot(a, b)
 
 def add(a,b):
     return a + b
+
 def subtract(a, b):
     return a - b
+
 def multiply(a, b):
     return a * b
+
 def divide(a , b):
     if a == 0:
         raise ZeroDivisionError("The first number a cannot be 0")
     return b / a
+
 def logarithm(a, b):
     if a <=0:
         raise ValueError("Input must be greater than 0")
     return log(b, a)
+
 def exponent(a, b):
     return a**b
